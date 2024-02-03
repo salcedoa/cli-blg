90cafcede8378dbdcd55d3ef3c282d597d689c11
salcedoa <andreas.salcedo-cespedes@city.ac.uk>
2024-02-03 16:04:03 +0000

    Added pagination with SQLAlchemy

--- backend/app.py
+++ backend/app.py
@@ -1,93 +1,64 @@
-from flask import Flask, request, render_template, g
+from flask import Flask, request, render_template
 from dotenv import dotenv_values
 from datetime import datetime
-import sqlite3, re
+from flask_sqlalchemy import SQLAlchemy
+import re, os
 
+db = SQLAlchemy()
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(basedir, "db/posts.db")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+db.init_app(app)
 
-# config for development
-app.config["TEMPLATES_AUTO_RELOAD"] = True
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.Text, nullable=True)
+    body = db.Column(db.Text, nullable=False)
+    postTime = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+
+# tables and models are created
+with app.app_context():
+    db.create_all()
 
 # Setting up variables from .env
 config = dotenv_values(".env")
 customTimeFormat = config["TIMEFORMAT"]
 name = config["NAME"]
+postsPerPage = int(config["POSTS_PER_PAGE"])
 
-# Set the main header of the web page
-if name != "": name = config["NAME"]
-elif name == "": name = "my"
-else: raise Exception("No name variable detected, please ensure that you set up the .env file")
-currentMonth = datetime.now().strftime("%B").lower() + " " + str(datetime.now().year)
-
-@app.route("/")
-def index():
+@app.route('/')
+def posts(page=1):
     # Query the database for all posts and return them in a JSON string object
-    return render_template("index.html",posts=fetchPosts(), name=name, date=currentMonth)
+    page = db.paginate(db.select(Posts).order_by(Posts.postTime.desc()), per_page=postsPerPage)
+    
+    # Reformat the text and date for display
+    for post in page.items:
+        post.body = parseBody(post.body)
+        post.postTime = convertTime(post.postTime)
+    
+    return render_template("index.html",page=page, name=name)
 
 @app.route('/json', methods=['POST'])
 def processPost():
     # Get the JSON data from request and create dictionary
     postObject = request.json
-    
-    # Insert the dictionary data into the database
-    conn = get_db()
-    c = conn.cursor()
-    c.execute('INSERT INTO posts (title, body, postTime) VALUES (?, ?, ?)', (postObject['title'], postObject['body'], postObject['postTime']))
-    conn.commit()
-    conn.close()
-    
-    return render_template('index.html', posts=fetchPosts(), name=name)
 
-@app.route('/archive')
-def handleArchive():
-    return render_template("archive.html", name=name, months=fetchMonths())
+    post = Posts(title=postObject['title'], body=postObject['body'], postTime=convertJsonToDatetime(postObject['postTime']))
+    db.session.add(post)
+    db.session.commit()
 
-# DATABASE FUNCTIONS
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('db/schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+    return 'New post added!'
 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect("db/posts.db")
-    return db
+# SQLite Datetime only accepts Python datetime, so the JSON request is converted
+def convertJsonToDatetime(jsonTime):
+    datetimeObject = datetime.strptime(jsonTime, "%Y-%m-%d %H:%M:%S")
+    return datetimeObject
 
 @app.teardown_appcontext
 def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
-# Returns all posts from the database
-def fetchPosts():
-    conn = get_db()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM posts ORDER BY postTime DESC")
-    rows = cursor.fetchall()
-    conn.close()
-    
-    # Store the query results into a json object (python list)
-    allPosts = [{'title': row[1], 'body': parseBody(row[2]), 'postTime': convertTime(row[3])} for row in rows]
-    return allPosts
-
-# Returns all unique month and year combinations from posts
-def fetchMonths():
-    conn = get_db()
-    cursor = conn.cursor()
-    cursor.execute("SELECT DISTINCT strftime('%m', postTime) AS month, strftime('%Y', postTime) AS year FROM posts")
-    rows = cursor.fetchall()
-    conn.close()
-
-    # Convert month number received from the SQLite query to month name
-    months = [list(row) for row in rows]
-    for m in months:
-        m[0] = datetime.strptime(m[0], '%m').strftime('%B').lower()
-    
-    return months
+    db.session.remove()
 
 # Returns an entry body which has been formatted to be displayed with HTML
 def parseBody(text):
@@ -103,15 +74,9 @@ def parseBody(text):
 
 # Returns the timestamp in the specified format
 def convertTime(fetchedDateTime):
-    print(fetchedDateTime)
-    formatTime = datetime.strptime(fetchedDateTime, '%Y-%m-%d %H:%M:%S')
-    displayTime = datetime.strftime(formatTime, customTimeFormat)
+    displayTime = datetime.strftime(fetchedDateTime, customTimeFormat)
     return displayTime
 
-# For filtering, this function returns the date and year of the post
-def getMonthYear(fetchedDateTime):
-    fullDateTime = datetime.strptime(fetchedDateTime, '%Y-%m-%d %H:%M:%S')
-    return fullDateTime.strftime('%B %Y')
-
 if __name__ == '__main__':
+    db.create_all()
     app.run()
